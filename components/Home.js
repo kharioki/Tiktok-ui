16ca7eb5f5a98a915d050a1851543ebdfdc25468
kharioki <antolisah@gmail.com>
2020-06-28 23:39:47 +0300

    displaying video details

--- components/Home.js
+++ components/Home.js
@@ -20,6 +20,49 @@ const VideoData = props => {
   return (
     <View>
       <Video source={props.item.video} repeat style={styles.video} />
+      <View style={styles.mainContainer}>
+        <View style={styles.innerLeft}>
+          <View style={styles.dataContainer}>
+            <Text style={styles.title}>@{props.item.title}</Text>
+            <Text style={styles.description} numberOfLines={4}>
+              {props.item.description}
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.innerRight}>
+          <ImageBackground
+            source={props.item.img}
+            style={styles.profile}
+            borderRadius={25}
+            borderWidth={1}
+            borderColor="#fff">
+            <TouchableOpacity style={styles.btn}>
+              <Icon name="ios-add" color="#fff" size={15} />
+            </TouchableOpacity>
+          </ImageBackground>
+
+          <Icon name="ios-heart" size={45} color="#e5e5e5" />
+          <Text style={{color: '#fff', marginBottom: 25}}>456</Text>
+
+          <Icon2 name="comment" size={45} color="#e5e5e5" />
+          <Text style={{color: '#fff', marginBottom: 25}}>123</Text>
+
+          <Icon
+            name="logo-whatsapp"
+            size={45}
+            color="#4fce5d"
+            style={{marginBottom: 25}}
+          />
+
+          <ImageBackground
+            source={require('../assets/p2.jpg')}
+            style={styles.profile}
+            borderRadius={25}
+            borderWidth={1}
+            borderColor="#fff"></ImageBackground>
+        </View>
+      </View>
     </View>
   );
 };
@@ -28,7 +71,7 @@ export default Home = () => {
   let data = [
     {
       id: '1',
-      title: 'AppDevBlog',
+      title: 'Tony Stark',
       video: require('../assets/sample.mp4'),
       img: require('../assets/p1.jpg'),
       description:
@@ -36,7 +79,7 @@ export default Home = () => {
     },
     {
       id: '2',
-      title: 'AppDevBlog',
+      title: 'Captain America',
       video: require('../assets/sample.mp4'),
       img: require('../assets/p1.jpg'),
       description:
@@ -44,7 +87,7 @@ export default Home = () => {
     },
     {
       id: '3',
-      title: 'AppDevBlog',
+      title: 'Hawkeye',
       video: require('../assets/sample.mp4'),
       img: require('../assets/p1.jpg'),
       description:
@@ -52,7 +95,7 @@ export default Home = () => {
     },
     {
       id: '4',
-      title: 'AppDevBlog',
+      title: 'Black Widow',
       video: require('../assets/sample.mp4'),
       img: require('../assets/p1.jpg'),
       description:
@@ -60,7 +103,7 @@ export default Home = () => {
     },
     {
       id: '5',
-      title: 'AppDevBlog',
+      title: 'Hulk',
       video: require('../assets/sample.mp4'),
       img: require('../assets/p1.jpg'),
       description:
@@ -76,6 +119,11 @@ export default Home = () => {
         renderItem={({item, index}) => <VideoData item={item} />}
         keyExtractor={item => item.id}
       />
+      <View style={styles.header}>
+        <Text style={styles.text}>Following</Text>
+        <Icon name="md-more" color="gray" size={15} style={{marginRight: 15}} />
+        <Text style={[styles.text, {color: 'gray'}]}>For You</Text>
+      </View>
     </View>
   );
 };
@@ -85,5 +133,68 @@ const styles = StyleSheet.create({
     height: height,
     width: width,
     backgroundColor: '#000',
+  },
+  header: {
+    width: width,
+    height: 50,
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    flexDirection: 'row',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 17,
+    marginRight: 15,
+  },
+  mainContainer: {
+    height: '55%',
+    flexDirection: 'row',
+    width: width,
+    position: 'absolute',
+    bottom: 0,
+  },
+  innerLeft: {
+    width: '80%',
+    height: '100%',
+  },
+  innerRight: {
+    width: '20%',
+    height: '100%',
+    alignItems: 'center',
+  },
+  profile: {
+    height: 50,
+    width: 50,
+    alignItems: 'center',
+    marginBottom: 25,
+  },
+  btn: {
+    backgroundColor: '#ff5b77',
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    elevation: 5,
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: -10,
+  },
+  dataContainer: {
+    height: '45%',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    padding: 5,
+  },
+  title: {
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  description: {
+    color: '#e5e5e5',
   },
 });
